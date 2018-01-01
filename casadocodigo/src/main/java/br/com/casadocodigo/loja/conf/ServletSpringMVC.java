7324c1ff456575d1c7f16de9c26cc9fa4e8906ff
Ramon Lacava <ramonrune@gmail.com>
2017-12-31 23:07:20 -0200

    Algumas modificações

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -1,9 +1,11 @@
 package br.com.casadocodigo.loja.conf;
 
 
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -34,5 +36,12 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	protected void customizeRegistration(Dynamic registration){
 		registration.setMultipartConfig(new MultipartConfigElement(""));
 	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new OpenEntityManagerInViewFilter()};
+	}
+	
+	
 
 }
